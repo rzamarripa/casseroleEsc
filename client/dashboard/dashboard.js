283edbf9230518b26f461ad5206273881c46ae38
Roberto Zamarripa <roberto@masoft.mx>
2017-01-11 01:48:39 -0700

    dashboard de gastos y demás

--- client/dashboard/dashboard.js
+++ client/dashboard/dashboard.js
@@ -7,6 +7,8 @@ function DashboardCtrl($scope, $meteor, $reactive, $state, toastr) {
 	window.rc = rc;
 	
 	this.alumnos_id = [];
+	this.conceptos_id = [];
+	this.semanaActual = moment(new Date()).isoWeek();
 
   this.subscribe("inscripciones",()=>{
 		return [{estatus : 1, campus_id : Meteor.user() != undefined ? Meteor.user().profile.campus_id : "" }]
@@ -23,6 +25,18 @@ function DashboardCtrl($scope, $meteor, $reactive, $state, toastr) {
 	this.subscribe('alumnos', () => {		
 		return [{_id : { $in : this.getCollectionReactively('alumnos_id')}}]
 	});
+	
+	this.subscribe('gastos', () => {
+    return [{estatus: true, semana: this.semanaActual, campus_id: Meteor.user() != undefined ? Meteor.user().profile.campus_id : ''}];
+  });
+  
+  this.subscribe('conceptosGasto', () => {
+    return [{_id : { $in : this.getCollectionReactively('conceptos_id')}}]
+  });
+  
+  this.subscribe('cuentas', () => {
+    return [{estatus: true, seccion_id: Meteor.user() != undefined ? Meteor.user().profile.seccion_id : ''}];
+  });
 
   this.helpers({
 	  inscripcionesActivas : () => {
@@ -64,6 +78,57 @@ function DashboardCtrl($scope, $meteor, $reactive, $state, toastr) {
 		  }
 		  console.log("arreglo", _.toArray( arreglo));
 		  return arreglo;
+	  },
+	  gastosCheque : () => {
+		  var gas = Gastos.find({tipoGasto  : "cheques"}).fetch();
+		  var total = 0.00;
+			if(gas != undefined){
+				_.each(gas, function(g){
+					total += g.importe;
+					rc.conceptos_id.push(g.concepto_id);
+					g.concepto = ConceptosGasto.findOne(g.concepto_id);
+				})
+				gas.push({chequeReferenciado : "Total", importe : total});
+			}
+			return gas;
+	  },
+	  gastosRelaciones : () => {
+		  var gas = Gastos.find({tipoGasto  : "relaciones"}).fetch();
+		  var total = 0.00;
+			if(gas != undefined){
+				_.each(gas, function(g){
+					total += g.importe;
+					rc.conceptos_id.push(g.concepto_id);
+					g.concepto = ConceptosGasto.findOne(g.concepto_id);
+				})
+				gas.push({registros : "Total", importe : total});
+			}
+			return gas;
+	  },
+	  gastosDepositos : () => {
+		  var gas = Gastos.find({tipoGasto  : "depositos"}).fetch();
+		  var total = 0.00;
+			if(gas != undefined){
+				_.each(gas, function(g){
+					total += g.importe;
+					g.cuenta = Cuentas.findOne(g.cuenta_id);
+				})
+				gas.push({cuenta : {nombre : "Total"}, importe : total});
+			}
+			return gas;
+	  },
+	  gastosAdmon : () => {
+		  var gas = Gastos.find({tipoGasto  : "admon"}).fetch();
+		  var total = 0.00;
+			if(gas != undefined){
+				_.each(gas, function(g){
+					total += g.importe;
+					rc.conceptos_id.push(g.concepto_id);
+					g.concepto = ConceptosGasto.findOne(g.concepto_id);
+				})
+				gas.push({concepto : { nombre : "Total"}, importe : total});
+			}
+			return gas;
 	  }
   });
 }
